4689402713933a589f2d9823dc25c21e8c4d1ffc
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-07-02 10:01:32 +0200

    update

--- Tarracommerce/models/pedido.js
+++ Tarracommerce/models/pedido.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-
 const orderSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' },
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Producto' }],
     total: Number,
     status: String,
 });
-
 module.exports = mongoose.model('Pedido', orderSchema);
